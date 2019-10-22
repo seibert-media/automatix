9f2698aab57b26d07372d97e26e5f0948e45d30a
Johannes Paul <jpaul@seibert-media.net>
2019-10-22 15:14:32 +0200

    Add setup.py and automatix directory

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='automatix',
+    version='0.0.1',
+    description='Automation wrapper for bash and python commands',
+    url='https://github.com/seibert-media/automatix',
+    author='Johannes Paul, //SEIBERT/MEDIA GmbH',
+    author_email='jpaul@seibert-media.net',
+    license='MIT',
+    python_requires='>=3.6',
+    install_requires=[
+        'pyyaml>=5.1',
+    ],
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'automatix=automatix:main',
+        ],
+    },
+)
