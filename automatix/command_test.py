de5323f8a97a21f892ddfcec7d6246e62d6be90a
Johannes Paul <jpaul@seibert-media.net>
2020-03-13 17:24:04 +0100

    Add tests for local_cmd and python_cmd

--- automatix/command_test.py
+++ automatix/command_test.py
@@ -15,3 +15,31 @@ def test__execute_remote_cmd(ssh_up):
         run_command_and_check('ssh docker-test ls /test_remote_cmd >/dev/null')
     except CalledProcessError:
         pytest.fail('Check for remote file not successful')
+
+
+def test__execute_local_cmd(capfd):
+    test_string = "Local Test String"
+
+    # empty captured stdin and stderr
+    _ = capfd.readouterr()
+
+    cmd = Command(pipeline_cmd={'local': f'echo {test_string}'}, index=2, env=environment)
+    cmd.execute()
+
+    out, err = capfd.readouterr()
+    assert test_string in out
+    assert err == ''
+
+
+def test__execute_python_cmd():
+    test_cmd = """
+from uuid import uuid4
+from pprint import pprint
+PERSISTENT_VARS.update(locals())
+"""
+
+    cmd = Command(pipeline_cmd={'python': test_cmd}, index=2, env=environment)
+    cmd.execute()
+
+    cmd = Command(pipeline_cmd={'python': 'print(uuid4())'}, index=2, env=environment)
+    cmd.execute()
