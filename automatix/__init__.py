e4f8da16aeacf6e0bdf54330801382d3bea14a44
Johannes Paul <jpaul@seibert-media.net>
2020-07-13 19:26:10 +0200

    Move things from __init__.py to config.py

--- automatix/__init__.py
+++ automatix/__init__.py
@@ -1,48 +1,13 @@
-import argparse
-import logging
 import os
-from collections import OrderedDict
 from copy import deepcopy
 from csv import DictReader
 from importlib import import_module
 from time import time
 from typing import List
 
-import yaml
-
 from .automatix import Automatix
 from .command import Command
-
-yaml.warnings({'YAMLLoadWarning': False})
-
-
-def read_yaml(yamlfile: str) -> dict:
-    with open(yamlfile) as file:
-        return yaml.load(file.read())
-
-
-SCRIPT_FIELDS = OrderedDict()
-SCRIPT_FIELDS['systems'] = 'Systems'
-SCRIPT_FIELDS['vars'] = 'Variables'
-
-CONFIG = {
-    'script_dir': '~/automatix-config',
-    'constants': {},
-    'encoding': os.getenv('ENCODING', 'utf-8'),
-    'import_path': '.',
-    'ssh_cmd': 'ssh {hostname} sudo ',
-    'remote_tmp_dir': 'automatix_tmp',
-    'logger': 'automatix',
-    'bundlewrap': False,
-    'teamvault': False,
-}
-
-configfile = os.path.expanduser(os.getenv('AUTOMATIX_CONFIG', '~/.automatix.cfg.yaml'))
-if os.path.isfile(configfile):
-    CONFIG.update(read_yaml(configfile))
-
-if os.getenv('AUTOMATIX_SCRIPT_DIR'):
-    CONFIG['script_dir'] = os.getenv('AUTOMATIX_SCRIPT_DIR')
+from .config import arguments, CONFIG, get_script, LOG, update_script_from_row, collect_vars, SCRIPT_FIELDS
 
 if CONFIG.get('logging_lib'):
     log_lib = import_module(CONFIG.get('logging_lib'))
@@ -59,129 +24,10 @@ if CONFIG.get('bundlewrap'):
 else:
     cmdClass = Command
 
-LOG = logging.getLogger(CONFIG['logger'])
-
-SCRIPT_PATH = os.path.expanduser(CONFIG['script_dir'])
-
-if CONFIG['teamvault']:
-    import bwtv
-
-    SCRIPT_FIELDS['secrets'] = 'Secrets'
-
-
-    class UnknownSecretTypeException(Exception):
-        pass
-
-
-def _arguments() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(
-        description='Automation wrapper for bash and python commands.',
-        epilog='Explanations and README at https://github.com/seibert-media/automatix',
-    )
-    parser.add_argument(
-        'scriptfile',
-        help='Path to scriptfile (yaml), use " -- " if needed to delimit this from argument fields',
-    )
-    for field in SCRIPT_FIELDS.keys():
-        parser.add_argument(
-            f'--{field}',
-            nargs='*',
-            help=f'Use this to set {field} without adding them to the script or to overwrite them. '
-                 f'You can specify multiple {field} like: --{field} v1=string1 v2=string2 v3=string3',
-        )
-    parser.add_argument(
-        '--vars-file',
-        help='Path to a CSV file containing variables for batch processing',
-    )
-    parser.add_argument(
-        '--print-overview', '-p',
-        action='store_true',
-        help='Just print command pipeline overview with indices then exit without executing the commandline. '
-             'Note that the *always pipeline* will be executed beforehand.',
-    )
-    parser.add_argument(
-        '--jump-to', '-j',
-        help='Jump to step instead of starting at the beginning',
-        default=0,
-    )
-    parser.add_argument(
-        '--interactive', '-i',
-        action='store_true',
-        help='Confirm actions before executing',
-    )
-    parser.add_argument(
-        '--force', '-f',
-        action='store_true',
-        help='try always to proceed (except manual steps), even if errors occur (no retries)'
-    )
-    parser.add_argument(
-        '--debug', '-d',
-        action='store_true',
-        help='activate debug log level',
-    )
-    return parser.parse_args()
-
-
-def _overwrite(script: dict, key: str, data: str):
-    script.setdefault(key, {})
-    for item in data:
-        k, v = item.split('=')
-        script[key][k] = v
-
-
-def get_script(args: argparse.Namespace) -> dict:
-    file = args.scriptfile
-    if not os.path.isfile(args.scriptfile):
-        file = f'{SCRIPT_PATH}/{args.scriptfile}'
-
-    script = read_yaml(file)
-
-    for field in SCRIPT_FIELDS.keys():
-        if vars(args).get(field):
-            _overwrite(script=script, key=field, data=vars(args)[field])
-
-    return script
-
-
-def collect_vars(script: dict) -> dict:
-    var_dict = script.get('vars', {})
-    script['vars'] = var_dict  # just for the case it was empty
-    if CONFIG['teamvault']:
-        for key, secret in script.get('secrets', {}).items():
-            sid, field = secret.split('_')
-            if field == 'password':
-                var_dict[key] = bwtv.password(sid)
-            elif field == 'username':
-                var_dict[key] = bwtv.username(sid)
-            elif field == 'file':
-                var_dict[key] = bwtv.file(sid)
-            else:
-                raise UnknownSecretTypeException(field)
-    for syskey, system in script.get('systems', {}).items():
-        var_dict[f'system_{syskey}'] = system
-    return var_dict
-
-
-def _update_script_from_row(row: dict, script: dict, index: int):
-    if not row:
-        return
-    try:
-        script['name'] += f" ({index} {row.pop('label')})"
-    except KeyError:
-        script['name'] += f" ({index})"
-    for key, value in row.items():
-        assert len(key.split(':')) == 2, \
-            'First row in CSV must contain "label" or the field name and key seperated by colons' \
-            ' like "label,systems:mysystem,vars:myvar".'
-        key_type, key_name = key.split(':')
-        assert key_type in SCRIPT_FIELDS.keys(), \
-            f'First row in CSV: Field name is \'{key_type}\', but has to be one of {list(SCRIPT_FIELDS.keys())}.'
-        script[key_type][key_name] = value
-
 
 def main():
     starttime = time()
-    args = _arguments()
+    args = arguments()
     init_logger(name=CONFIG['logger'], debug=args.debug)
 
     script = get_script(args=args)
@@ -197,7 +43,7 @@ def main():
         i += 1
 
         script_copy = deepcopy(script)
-        _update_script_from_row(row=row, script=script_copy, index=i)
+        update_script_from_row(row=row, script=script_copy, index=i)
 
         variables = collect_vars(script_copy)
 
