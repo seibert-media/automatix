3a17cb5db71c1d85d516b02c425d067db9a8d5d6
Johannes Paul <37707236+j-p-a-u-l@users.noreply.github.com>
2022-01-04 16:11:58 +0100

    automatix/bash_completion.py: Fix directory completion

--- automatix/bash_completion.py
+++ automatix/bash_completion.py
@@ -20,7 +20,7 @@ class ScriptFileCompleter(object):
         completion = []
         pre_len = len(self.script_path)+1
         directories = _call(["bash", "-c", f"compgen -A directory -- '{self.script_path}/{prefix}'"])
-        completion += [f'{d}/' for d in directories]
+        completion += [f'{d[pre_len:]}/' for d in directories]
         for ext in ['yaml', 'yml']:
             files = _call(["bash", "-c", f"compgen -A file -X '!*.{ext}' -- '{self.script_path}/{prefix}'"])
             completion += [f[len(self.script_path)+1:] for f in files]
