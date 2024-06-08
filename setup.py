f8cfd92c82ea37e64159dd8dba1dc4a090797dd2
Johannes Paul <vanadinit@quantentunnel.de>
2024-06-08 19:54:17 +0200

    Release 2.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='automatix_cmd',
-    version='2.0.1',
+    version='2.1.0',
     description='Automation wrapper for bash and python commands',
     keywords=['bash', 'shell', 'command', 'automation', 'process', 'wrapper', 'devops', 'system administration'],
     long_description=long_description,
