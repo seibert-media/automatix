514f64b1c40b7705d4d4697d14bed42c1b45dc09
Johannes Paul <johannes.paul@seibert.group>
2025-04-03 12:17:40 +0200

    Add selection for jump-to is empty

--- automatix/helpers.py
+++ automatix/helpers.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import yaml
 
 yaml.warnings({'YAMLLoadWarning': False})
@@ -6,3 +8,25 @@ yaml.warnings({'YAMLLoadWarning': False})
 def read_yaml(yamlfile: str) -> dict:
     with open(yamlfile) as file:
         return yaml.load(file.read(), Loader=yaml.SafeLoader)
+
+
+def selector(entries: List[tuple], message: str = 'Found multiple entries, please choose:'):
+    """Provides a command line interface for selecting from multiple entries
+    :param entries: List of Tuples(entry: Any, label: str)
+    :param message: message to be displayed for selection
+    """
+    match len(entries):
+        case 0:
+            return None
+        case 1:
+            return entries[0][0]
+        case _:
+            entry_labels = '\n'.join([
+                f'{index}: {entry[1]}'
+                for index, entry in enumerate(entries)
+            ])
+            try:
+                return entries[int(input(f'\n{message} \n{entry_labels}\n\nYour choice:\n'))][0]
+            except (ValueError, IndexError):
+                print('Invalid answer! Please try again and type the number of your desired answer.')
+                return selector(entries)
